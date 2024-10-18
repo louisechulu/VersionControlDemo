5e0e50713378468a401f99218223687c946f5861
Your Name <lchulu@dmacc.edu>
2024-10-18 18:32:26 -0500

    Initial commit

---
+++ OneDrive/Desktop/cscorner/CIS152/src/main/java/com/mycompany/cis152/CIS152.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cis152;
+
+/**
+ *
+ * @author louch
+ */
+public class CIS152 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Class152");
+    }
+}
